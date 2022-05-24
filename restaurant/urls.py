36450fff75fc5552c361a05ab537db0fe7f09f9c
heresssy <petertomasko6@gmail.com>
2022-05-24 16:18:27 +0200

    [cleanup] refactoring

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,12 +5,17 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     path('create-order/<str:pk>/', views.create_order, name="create-order"),
-    path('view-order/<str:pk>/', views.view_order, name="view-order"),
-
-    path('view-table/<str:pk>/', views.view_table, name="view-table"),
     path('update-table/<str:pk>/', views.update_table, name="update-table"),
+
+    path('view-order/<str:pk>/', views.view_order, name="view-order"),
+    path('view-table/<str:pk>/', views.view_table, name="view-table"),
+
+    path('delete-order/<str:pk>/', views.delete_order, name="delete-order"),
+    path('delete-table/<str:pk>/', views.delete_table, name="delete-table"),
+
+    path('login/', views.login_view, name="login-view"),
+    path('logout/', views.logout_view, name="logout-view"),
+
     path('create-basic-menu/', views.create_basic_menu, name="create-basic-menu"),
-    path('login/', views.login_page, name="login-page"),
-    path('logout/', views.logout_page, name="logout-page"),
 
 ]
