57874328c31eb456525ccc709fcc63269b15d239
heresssy <petertomasko6@gmail.com>
2022-06-02 10:11:26 +0200

    add initialisation of basic groups

--- restaurant/apps.py
+++ restaurant/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 class RestaurantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restaurant'
 
-
+    def ready(self):
+        from django.contrib.auth.models import Group
+        for group in ['hostess', 'cook', 'server', 'manager']:
+            Group.objects.get_or_create(name=group)
